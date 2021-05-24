b58a38061e0ad3dba43b610d15d50b00949f4257
user <user@DESKTOP-72M7BDB>
2021-05-24 22:03:42 +0530

    first commit

---
+++ githubdemo1/src/main/java/com/pages/loginPage.java
@@ -0,0 +1,12 @@
+package com.pages;
+
+public class loginPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is the loginpage method");
+		System.out.println("this is the changed method");
+
+	}
+
+}
